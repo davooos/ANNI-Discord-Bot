366270d5f68b9cb5324be425b398657f874897d6
ner216 <nerlandia508@gmail.com>
2024-10-24 18:59:08 -0400

    sync repo

--- cogs/view.py
+++ cogs/view.py
@@ -7,9 +7,25 @@ from datetime import timezone
 class view(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
+	
+	@commands.command()
+	async def getData(self, ctx, member: discord.Member = None) -> None:
+		members = ctx.guild.members
+		memberData = dict()
+		roleData = dict()
+	
+		
+		for member in members:
+			roleData[member.id] = member.roles
+			memberData[member.id] = member
+		
+		print(roleData)
+		print()
+		print(members)
+		
 		
 	@commands.command(name="view", description="View server member data")
-	async def view(self, ctx):
+	async def view(self, ctx) -> None:
 		data = str()
 		guild = ctx.guild
 		members = guild.members
