504c69b262cdcad5b4b37ebd7973ece95f514809
Nolan <provencher.nolan@gmail.com>
2024-10-25 12:45:17 -0400

    Made progress on member configs and view cog

--- cogs/chatbot.py
+++ cogs/chatbot.py
@@ -8,44 +8,84 @@ from datetime import tzinfo, timedelta, datetime, timezone
 class chatbot(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-		
+
 	@commands.command(name="ask", description="ask the bot a question")
 	async def ask(self, ctx) -> None:
-	    questions = dict(helpers.loadConfig("questions"))
-	    data = str()
-	    tokens = ctx.message.content.split()
+		getQuestions = ["all", "print", "show", "questions", "quest", "help"] #command tokens for showing quesitons
+		positions = ["moderator","graphic designer", "animator", "software engineer", "communications"]
+		leaders = ["chief", "officer", "manager"]
+		questions = ["When does my internship end?", "What team am I part of?", "Who are the moderators?", "Who are my team leaders?"]
+		data = str()
+		tokens = ctx.message.content.split()
 	    
-	    if len(tokens) < 2:
-	        for key in questions:
-	            data = data + str(key) + ": " + str(questions[key]) + "\n"
-	        data = data + "Enter !ask + the number of the question you wish to ask me!"
+		#command interpretation per token(word)
+		if len(tokens) < 2 or tokens[1] in getQuestions:
+			for i,quest in enumerate(questions):
+				data = data + str(i+1) + ": " + str(quest) + "\n"
+			data = data + "Enter !ask + the number repreasenting the question you wish to ask me!"
 
-	    elif len(tokens) < 3:
-	    	if tokens[1].isdigit() == True:
-	    		if tokens[1] == '1':
-	    			period = timedelta(weeks=16)
-	    			join_date = ctx.author.joined_at
-	    			cur_date = datetime.now(timezone.utc)
-	    			end_date = join_date + period #overload operator for datetime that returns timedelta obj
-	    			time_till_end = end_date - cur_date
-	    			joinStamp = helpers.getTimeStamp(join_date)
-	    			endStamp = helpers.getTimeStamp(end_date)
-	    			if time_till_end.seconds > 0:
-	    				data = data + "You joined " + str(joinStamp) + " and your intership ends " + str(endStamp) + "." + "\n"
-	    				data = data + "You have " + str(int(time_till_end.days / 7)) + " weeks and " + str(time_till_end.days % 7) + " days left."
-	    			else:
-	    				data = data + "Your internship ended " + str(end_date)
+		elif len(tokens) < 3:
+			if tokens[1].isdigit() == True:
+				if tokens[1] == '1':
+					period = timedelta(weeks=16)
+					join_date = ctx.author.joined_at
+					cur_date = datetime.now(timezone.utc)
+					end_date = join_date + period #overload operator for datetime that returns timedelta obj
+					time_till_end = end_date - cur_date
+					joinStamp = helpers.getTimeStamp(join_date)
+					endStamp = helpers.getTimeStamp(end_date)
+					if time_till_end.seconds > 0:
+						data = data + "You joined " + str(joinStamp) + " and your intership ends " + str(endStamp) + "." + "\n"
+						data = data + "You have " + str(int(time_till_end.days / 7)) + " weeks and " + str(time_till_end.days % 7) + " days left."
+					else:
+						data = data + "Your internship ended " + str(end_date)
 
-	    		elif tokens[1] == '2':
-	    			data = data + "Your roles in this server include: \n"
-	    			for role in ctx.author.roles:
-	    				if role.name != "@everyone":
-	    					data = data + role.name + "\n"
-	    		else:
-	    			print("Error: invalid question [chatbot::ask]")
-	    			data = "Sorry, that number does not match any of my questions."
+				elif tokens[1] == '2':
+					teamfound = bool(False)
+					data = data + "Your team in this server: \n"
+					for role in ctx.author.roles:
+						if "team" in role.name.lower():
+							data = data + role.name + "\n"
+							teamfound = True
+					if teamfound == False:
+						data = "Sorry, I was not able to determine your team."
+
+				elif tokens[1] == '3':
+					modfound = bool(False)
+					data = data + "The server moderators are: \n"
+					members = ctx.guild.members
+					for member in members:
+						for role in member.roles:
+							if "moderat" in role.name.lower():
+								data = date + member.global_name + "\n"
+								modfound = True
+					if modfound == False:
+						data = "Sorry, I was unable to find the moderators."
+
+				elif tokens[1] == '4':
+					authorTeams = list()
+					leadersFound = bool(False)
+					data = "Your team leaders are: \n"
+
+					for role in ctx.author.roles:
+						if "team" in role.name.lower():
+							authorTeams.append(role.name.lower())
+
+					for member in ctx.guild.members:
+						for role in member.roles:
+							for team in authorTeams:
+								if team in role.name:
+									data = data + member.global_name + "\n"
+									leadersFound = True
+
+					if len(authorTeams) < 1 or leadersFound == False:
+						data = "Sorry, I was unable to find your team leaders."
+
+				else:
+					print("Error: invalid question [chatbot::ask]")
+					data = "Sorry, that number does not match any of my questions."
 	    
-	    await ctx.send(data)
+		await ctx.send(data)
 	
 
 async def setup(bot):
--- cogs/view.py
+++ cogs/view.py
@@ -4,9 +4,123 @@ from discord.ext import commands
 import datetime
 from datetime import timezone
 
+class people(object):
+	def __init__(self):
+
+	def createLog(self) -> None:
+
+
 class view(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
+
+	async def createLog(self, ctx) -> None:
+		log = dict() #dictionary for members
+		memData = dict() #dictionary for individual member data
+		members = ctx.guild.members #THIS MAY BE ONLY FOR CACHED MEMBERS IN SERVER --- PLEASE CHECK LATER
+
+		period = timedelta(weeks=16)
+		join_date = ctx.author.joined_at
+		end_date = join_date + period #overload operator for datetime that returns timedelta obj
+
+		for member in members:
+			if member.bot == False:
+				memData["name"] = member.global_name
+				memData["intern"] = False
+				memData["startDate"] = join_date
+				memData["endDate"] = end_date
+
+				log[member.id] = memberData
+				memData = dict()
+
+		helpers.saveConfig("members", log)
+
+	@commands.command()
+	async def memc(self, ctx):
+		await memberconfig(ctx)
+
+	@commands.command()
+	async def memberconfig(self, ctx):
+		log = helpers.loadConfig("members")
+		helpTokens = ["all", "print", "show", "questions", "quest", "help"]
+		internTrueTokens = ["yes", "true","in"]
+		internFalseTokens = ["no", "false", "end","over","out"]
+		dateTokens = ["makedate", "date", "this", "thisdate", "now"]
+
+		tokens = ctx.message.content.split()
+		data = str()
+
+		#temp variables
+		memberid = int()
+		memberName = str()
+		memberidx = int()
+		newValue = None
+		field = str()
+
+		#flag variables
+		memberFound = bool(False)
+		validField = bool(False)
+		stop = bool(False)
+
+
+		if len(tokens) < 2 or tokens[1].lower() in helpTokens:
+			data = "Enter !memberconfig member field data"
+		elif len(tokens) > 1:
+			if tokens[1].isdigit():
+				for i,member in enumerate(log.keys()):
+					if int(tokens[1]) == int(member) or int(tokens[1]-1) == i:
+						memberid = member
+						memberFound = True
+						print("Member found in config as " + member + " [view::memberConfig]")
+			else:
+				for member in log:
+					if tokens[1] == member["name"]:
+						memberid = member
+						memberFound = True
+						print("Member found in config as " + member + " [view::memberConfig]")
+
+			if len(tokens) > 2:
+				for f in log[0]:
+					if tokens[2] == f:
+						field = tokens[2]
+						validField = True
+
+				if validField == True:
+					if len(tokens) > 3:
+						newValue == tokens[3]
+		else:
+			print("Error: insufficient arguments given [view::memberconfig]")
+
+		if newValue not None and validField == True: #checks to see if a new value and valid field were given
+			if field == "intern":
+				if newValue in internTrueTokens:
+					newValue = bool(True)
+				elif newValue in internFalseTokens:
+					newValue = bool(False)
+				else:
+					print("Error: invalid value given for intern status [view::memberconfig]")
+					stop = True
+			elif field == "startDate" or field == "endDate":
+				cur_date = datetime.now(timezone.utc)
+				if newValue in dateTokens:	
+					newValue = cur_date
+				else:
+					print("Error: invalid token for start date argument [view::memberconfig]")
+					stop = True
+
+		if stop != True:
+			log[memberid][field] = newValue #update log if parameters are valid
+
+		try:
+			helpers.saveConfig("members", log) #save modified config file
+			print("Successfully saved config file [view::memberconfig]")
+			data = "I successfully updated the member data!"
+		except:
+			print("Error: could not save new config [view::memberconfig]")
+			data = "Sorry, I could not save this data change."
+
+		await ctx.send(data) #send end message at end of function
+
 	
 	@commands.command()
 	async def getData(self, ctx, member: discord.Member = None) -> None:
